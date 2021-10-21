f00ed0a91cd1f3764cbb1454542f31860ee4d975
felipe-alves142 <77590921+felipe-alves142@users.noreply.github.com>
2021-10-21 15:21:41 -0300

    first commit

---
+++ src/test/java/com/example/ecomercegamesmobile/EcomercegamesmobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecomercegamesmobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomercegamesmobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
